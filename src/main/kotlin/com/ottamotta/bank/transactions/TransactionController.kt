4a528d08dbbd52d31d72759df0f9ca6849fa2e87
Rodion Altshuler <altshuler.rodion@gmail.com>
2020-10-08 09:51:43 +0200

    REST endpoints: Deposit money into a specified bank account, Transfer some money across two bank accounts

---
+++ src/main/kotlin/com/ottamotta/bank/transactions/TransactionController.kt
@@ -0,0 +1,37 @@
+package com.ottamotta.bank.transactions
+
+import com.ottamotta.bank.account.Money
+import com.ottamotta.bank.transactions.processing.TransactionService
+import org.iban4j.Iban
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/transfers")
+class TransactionController(private val transactionService: TransactionService) {
+
+    data class DepositRequest(val to: Iban, val amount: Money)
+
+    @PostMapping("/deposit")
+    fun deposit(@RequestBody request: DepositRequest): Transaction {
+        return transactionService.submit(TransferCommand(to = request.to, amount = request.amount))
+    }
+
+    data class WithdrawRequest(val from: Iban, val amount: Money)
+
+    @PostMapping("/withdraw")
+    fun withdraw(@RequestBody request: WithdrawRequest): Transaction {
+        return transactionService.submit(TransferCommand(from = request.from, amount = request.amount))
+    }
+
+    data class TransferRequest(val from: Iban, val to: Iban, val amount: Money)
+
+    @PostMapping("/transfer")
+    fun transfer(@RequestBody request: TransferRequest): Transaction {
+        return transactionService.submit(TransferCommand(from = request.from, to = request.to, amount = request.amount))
+    }
+
+
+}
